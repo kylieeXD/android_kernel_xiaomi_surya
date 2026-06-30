4b8fd68facd2d69cbaa376e0e4183297f142a7b9
Richard Raya <rdxzv.dev@gmail.com>
2026-06-30 10:46:11 +0700

    syscall: Dynamically spoof uname version
    Co-authored-by: Tim Zimmermann <tim@linux4.de>
    Co-authored-by: basamaryan <basam.aryan@gmail.com>
    Co-authored-by: backslashxx <118538522+backslashxx@users.noreply.github.com>
    Co-authored-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: kylieeXD <khayloaftck@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1182,13 +1182,42 @@ static int override_release(char __user *release, size_t len)
 	return ret;
 }
 
+extern bool is_legacy_ebpf;
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
+	struct task_struct *t;
+	bool is_netmgrd = false;
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
+	if (!is_legacy_ebpf) {
+		if (!strncmp(current->comm, "bpfloader", 9) ||
+			!strncmp(current->comm, "netbpfload", 10) ||
+			!strncmp(current->comm, "netd", 4)) {
+			if (current_uid().val == 0) {
+				strcpy(tmp.release, "5.10.239");
+				pr_debug("fake uname: %s/%d release=%s\n",
+					current->comm, current->pid, tmp.release);
+			}
+		}
+	}
 	up_read(&uts_sem);
+
+	rcu_read_lock();
+	for_each_thread(current, t) {
+		if (thread_group_leader(t)) {
+			is_netmgrd = !strcmp(t->comm, "netmgrd");
+			break;
+		}
+	}
+	rcu_read_unlock();
+
+	if (is_netmgrd)
+		snprintf(tmp.release, sizeof(tmp.release), "%u.%u.%u-qgki",
+			 (u8)((LINUX_VERSION_CODE >> 16) & 0xff), (u8)((LINUX_VERSION_CODE >> 8) & 0xff),
+			 (u16)(LINUX_VERSION_CODE & 0xffff));
+
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
 
